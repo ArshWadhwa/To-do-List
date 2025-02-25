d19e0e97de6dec5833dde6ec4b3e25877f19a131
Arsh <arshwadhwa.2006@gmail.com>
2025-02-25 23:16:18 +0530

    Add Render build script

--- .render-build.sh
+++ .render-build.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
 ./gradlew build -x test
+
+#!/usr/bin/env bash
+
+# Install Java (OpenJDK 17)
+apt-get update && apt-get install -y openjdk-17-jdk
+
+# Set JAVA_HOME
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+
+# Proceed with the build
+./gradlew build -x test
