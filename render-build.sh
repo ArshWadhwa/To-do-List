36782125dcd4c1a8b2535bd71c3618d05555478a
Arsh <arshwadhwa.2006@gmail.com>
2025-02-25 23:25:36 +0530

    changes made render

--- render-build.sh
+++ render-build.sh
@@ -1,14 +1,11 @@
-#!/usr/bin/env bash
-./gradlew build -x test
+#!/bin/bash
 
-#!/usr/bin/env bash
-
-# Install Java (OpenJDK 17)
-apt-get update && apt-get install -y openjdk-17-jdk
-
-# Set JAVA_HOME
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+# Set correct Java path
+export JAVA_HOME=/opt/render/.jdk
 export PATH=$JAVA_HOME/bin:$PATH
 
-# Proceed with the build
+# Grant execute permission (just in case)
+chmod +x ./gradlew
+
+# Run the build command
 ./gradlew build -x test
